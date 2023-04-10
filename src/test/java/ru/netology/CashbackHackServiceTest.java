8bce189c033e6514757eb5ae5123f2903bb85a96
Marina Kuchmasova <kuchmasova.m@gmail.com>
2023-04-10 23:54:37 +0300

    add branch junit4,corrected build.gradle, added tests

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,12 +1,11 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.junit.Test
     public void testRemainWithCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
@@ -16,20 +15,20 @@ public class CashbackHackServiceTest {
 
     }
 
-    @Test
-    public void testEqualBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(actual, expected);
-    }
-    @Test
-    public void testMoreBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1001;
-        int actual = service.remain(amount);
-        int expected = 999;
-        assertEquals(actual, expected);
+        @Test
+        public void testEqualBoundary() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1000;
+            int actual = service.remain(amount);
+            int expected = 0;
+            assertEquals(actual, expected);
+        }
+        @Test
+        public void testMoreBoundary() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1001;
+            int actual = service.remain(amount);
+            int expected = 999;
+            assertEquals(actual, expected);
     }
 }
